05f5d5b6a473953b619bc2c643551d623c150b2c
Thomas An <tommyan97@msn.com>
2020-09-17 00:36:55 -0700

    added random questions to show up in sequence and an indicator to show if the right answer was chosen

--- script.js
+++ script.js
@@ -22,14 +22,23 @@ var questions = [
 
 var answeredQuestions = [];
 
-
-var timeLeft = questions.length * 10;
 var question = document.querySelector("#title-and-questions");
 var description = document.querySelector("#description");
 var start = document.querySelector("#start");
 var answerList = document.querySelector("#answer-choices");
+var time = document.querySelector("#time");
+var rightWrong = document.querySelector("#right-wrong");
+var enterScore = document.querySelector("#enter-score");
 
 var questionNum = 1;
+var timeLeft = questions.length * 15;
+var highScores = [];
+
+if (localStorage.getItem("scoreList") !== null) {
+
+    highScores = localStorage.getItem(scoreList);
+
+}
 
 start.addEventListener("click", function() {
 
@@ -39,21 +48,68 @@ start.addEventListener("click", function() {
 
 answerList.addEventListener("click", function(event) {
 
-    if (questions.length < 1) {
+    if (event.target.tagName === "BUTTON") {
 
-        displayScore();
+        var result = "Wrong!"
 
-    } else {
+        if (event.target.id === "#correct") {
+
+            result = "Correct!"
+
+        } else {
+
+            timeLeft -= 10;
+
+        }
+
+        rightWrong.innerHTML = "<hr>" + result;
+
+        var waitTime = 1;
+        var interval = setInterval (function() {
+
+            if (waitTime < 1) {
+
+                rightWrong.innerHTML = "";
+                clearInterval(interval);
+
+            }
+
+            waitTime--
+
+        }, 1000)
+
+        answerList.innerHTML = "";
+
+        if (questions.length > 0) {
+
+            newQuestion();
+
+        } else {
+
+            inputScore();
+
+            if (event.target.id === "#submit") {
 
 
 
-    }
+            }
+
+        }
+
+    } 
+
+
+})
+
+enterScore.addEventListener("click", function(event) {
+    
+    event.preventDefault();
 
 })
 
 function startGame() {
 
-    description.remove();
+    description.textContent = "";
     start.remove();
     question.style.textAlign = "left";
 
@@ -62,6 +118,9 @@ function startGame() {
 }
 
 function newQuestion() {
+
+    console.log(questions);
+    console.log(answeredQuestions);
 
     var answerBank = [];
     var randIndex = Math.floor(Math.random() * questions.length);
@@ -90,18 +149,31 @@ function newQuestion() {
 
     while (answerBank.length !== 0) {
 
-        var randIndex = Math.floor(Math.random() * answerBank.length);
-        answerBank[randIndex].textContent = answerNum + ". " +answerBank[randIndex].textContent;
-        answerList.appendChild(answerBank[randIndex]);
-        answerBank.splice(randIndex, 1);
+        var randInd = Math.floor(Math.random() * answerBank.length);
+        answerBank[randInd].textContent = answerNum + ". " +answerBank[randInd].textContent;
+        answerList.appendChild(answerBank[randInd]);
+        answerBank.splice(randInd, 1);
         answerNum++;
 
     }
 
-    answeredQuestions.unshift(questions.splice(randIndex, 1));
+    answeredQuestions.unshift(questions.splice(randIndex, 1)[0]);
 
 }
 
-function displayScore() {
+function inputScore() {
+
+    var name = document.createElement("input");
+    var submit = document.createElement("button");
+    submit.id = "#submit";
+    submit.className = "btn";
+    submit.textContent = "Submit";
+    name.setAttribute("maxlength", "3");
+    question.style.textAlign = "";
+    question.textContent = "Game Over!";
+    description.textContent = "Your final score is " + timeLeft;
+    enterScore.textContent = "Enter initials: ";
+    enterScore.appendChild(name);
+    enterScore.appendChild(submit);
 
 }
